bfea0f2df6af1a3984543db9410b6dd00a9450cd
roman8361 <torex@inbox.ru>
2018-11-23 16:35:44 +0300

    Initial commit

---
+++ desktop/src/ru/kravchenko/android/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.kravchenko.android.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.kravchenko.android.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
