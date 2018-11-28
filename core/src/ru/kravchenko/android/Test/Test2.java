89c029a5a862b9e9e0f1a233376bb21c325ece28
roman8361 <torex@inbox.ru>
2018-11-28 17:05:48 +0300

    Test to Git

---
+++ core/src/ru/kravchenko/android/Test/Test2.java
@@ -0,0 +1,7 @@
+package ru.kravchenko.android.Test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test for GIT");
+    }
+}
--- core/src/ru/kravchenko/android/screen/MenuScreen.java
+++ core/src/ru/kravchenko/android/screen/MenuScreen.java
@@ -93,4 +93,6 @@ public class MenuScreen extends Base2DScreen {
         System.out.println(" Tm = " + stopDotX);
     }
 
+
+
 }
